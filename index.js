46e857b64ae0924f5241a171fdc5ba113f44ff9c
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-04-04 17:00:48 -0700

    added attack damage/health reduction

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ class Sprite {
     };
     this.color = color;
     this.isAttacking;
+    this.health = 100;
   }
 
   draw() {
@@ -153,7 +154,9 @@ function animate() {
     player.isAttacking
   ) {
     player.isAttacking = false;
-    document.querySelector("#enemyHealth").style.width = "20%";
+    enemy.health -= 20;
+    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    console.log("player 1 attacking");
   }
 
   if (
@@ -161,6 +164,8 @@ function animate() {
     enemy.isAttacking
   ) {
     enemy.isAttacking = false;
+    player.health -= 20;
+    document.querySelector("#playerHealth").style.width = player.health + "%";
     console.log("player 2 attacking");
   }
 }
