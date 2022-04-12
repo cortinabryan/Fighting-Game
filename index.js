379f0c4e0b5ac5140d1d52664f2c2237d0c1c19c
cortinabryan <64225493+cortinabryan@users.noreply.github.com>
2022-04-12 16:25:22 -0700

    added jump and attack sprites

--- index.js
+++ index.js
@@ -55,6 +55,18 @@ const player = new Fighter({
       imageSrc: "./img/samuraiMack/Run.png",
       framesMax: 8,
     },
+    jump: {
+      imageSrc: "./img/samuraiMack/Jump.png",
+      framesMax: 2,
+    },
+    fall: {
+      imageSrc: "./img/samuraiMack/Fall.png",
+      framesMax: 2,
+    },
+    attack1: {
+      imageSrc: "./img/samuraiMack/Attack1.png",
+      framesMax: 6,
+    },
   },
 });
 
@@ -109,13 +121,20 @@ function animate() {
   enemy.velocity.x = 0;
 
   // player movement
-  player.image = player.sprites.idle.image;
+  player.switchSprite("idle");
   if (keys.a.pressed && player.lastKey === "a") {
     player.velocity.x = -5;
-    player.image = player.sprites.run.image;
+    player.switchSprite("run");
   } else if (keys.d.pressed && player.lastKey === "d") {
     player.velocity.x = 5;
-    player.image = player.sprites.run.image;
+    player.switchSprite("run");
+  }
+
+  // Jumping
+  if (player.velocity.y < 0) {
+    player.switchSprite("jump");
+  } else if (player.velocity.y > 0) {
+    player.switchSprite("fall");
   }
 
   // enemy movement
